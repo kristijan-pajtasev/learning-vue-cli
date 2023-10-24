26633a51136cfcc0a5ba2cd0ac42ba8780355524
kristijan-pajtasev <kristijan.pajtasev@hotmail.com>
2023-10-24 22:10:10 +0100

    fixed import

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 import './assets/main.css'
 
-import { createApp } from 'vue/dist/vue.runtime.esm-bundler'
+import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import PersonDetails from '@/components/PersonDetails.vue';
