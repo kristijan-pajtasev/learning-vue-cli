497132be32c29ef5a55cf64dc4a6c357680d9cb6
kristijan-pajtasev <kristijan.pajtasev@hotmail.com>
2023-10-31 20:34:13 +0000

    initial components emit view

---
+++ src/views/ComponentsEmitExampleView.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="about">
+    <h1>Props example</h1>
+    <div>
+      <Greeting name="John" />
+      <Greeting :name="name" />
+      <Greeting v-bind:name="name" />
+    </div>
+  </div>
+</template>
+
+<style>
+@media (min-width: 1024px) {
+  .about {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+  }
+}
+</style>
+<script>
+import Greeting from "../components/Greeting.vue";
+export default {
+  components: {Greeting},
+  data() {
+    return { name: "John" }
+  },
+  mounted() {
+    const _this = this;
+    setTimeout(() => {
+      _this.name = "Jane"
+    }, 3000)
+    console.log("mounted")
+  }
+}
+</script>
