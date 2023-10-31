def29010dcd102916011f34ba9692c31f00a5f25
kristijan-pajtasev <kristijan.pajtasev@hotmail.com>
2023-10-31 20:50:43 +0000

    button component

---
+++ src/components/CounterButton.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <button type="button" @click="handleClick">Click</button>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {}
+  },
+  methods: {
+    handleClick() {
+      console.log("handle click")
+    }
+  }
+}
+</script>
