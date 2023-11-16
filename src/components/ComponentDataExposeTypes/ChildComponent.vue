1563d5f1ba244283f69553d28a8e1e09c518fbda
kristijan-pajtasev <kristijan.pajtasev@hotmail.com>
2023-11-16 22:40:05 +0000

    computed

--- src/components/ComponentDataExposeTypes/ChildComponent.vue
+++ src/components/ComponentDataExposeTypes/ChildComponent.vue
@@ -1,15 +1,52 @@
 <template>
   <div>
-    child
+    <div>
+      <div>data</div>
+      <div>first: {{first}}</div>
+      <div>second: {{second}}</div>
+    </div>
+    <div>
+      <div>method</div>
+      <div>first: {{firstMethod()}}</div>
+      <div>second: {{secondMethod()}}</div>
+    </div>
+    <div>
+      <div>computed</div>
+      <div>first: {{firstComputedMethod}}</div>
+      <div>second: {{secondComputedMethod}}</div>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
+  props: ["firstCounter", "secondCounter"],
   data() {
-    return {}
+    const {firstCounter, secondCounter} = this.$props;
+    return {
+      first: firstCounter,
+      second: secondCounter,
+    }
   },
   methods: {
+    firstMethod() {
+      console.log("firstMethod")
+      return this.$props.firstCounter
+    },
+    secondMethod() {
+      console.log("secondMethod")
+      return this.$props.secondCounter
+    }
+  },
+  computed: {
+    firstComputedMethod() {
+      console.log("firstComputedMethod")
+      return this.$props.firstCounter
+    },
+    secondComputedMethod() {
+      console.log("secondComputedMethod")
+      return this.$props.secondCounter
+    }
   }
 }
 </script>
