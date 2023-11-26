5d721f70e8b57bd12c9fc3f98c6495204bf39a51
kristijan-pajtasev <kristijan.pajtasev@hotmail.com>
2023-11-26 23:04:18 +0000

    slots example route

---
+++ src/views/SlotExampleView.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="about">
+    <h1>Slots example</h1>
+  </div>
+</template>
+
+<style>
+@media (min-width: 1024px) {
+}
+</style>
+<script>
+export default {
+  components: {},
+  data() {
+    return {  }
+  },
+}
+</script>
