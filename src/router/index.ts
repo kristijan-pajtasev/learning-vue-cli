7caf1df5d241affe9c267ec70893051b1d32a261
kristijan-pajtasev <kristijan.pajtasev@hotmail.com>
2023-12-03 20:43:51 +0000

    slots advanced route and ivew

--- src/router/index.ts
+++ src/router/index.ts
@@ -48,6 +48,14 @@ const router = createRouter({
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
       component: () => import('../views/SlotExampleView.vue')
+    },
+    {
+      path: '/slots-advanced',
+      name: 'slotsAdvancedExample',
+      // route level code-splitting
+      // this generates a separate chunk (About.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () => import('../views/SlotAdvancedView.vue')
     }
   ]
 })
---
+++ src/views/SlotAdvancedView.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="about">
+    <h1>Slots example</h1>
+    <PopUpMessage>
+      <template #header>PopUp message header</template>
+      <template #default>Content</template>
+    </PopUpMessage>
+  </div>
+</template>
+
+<style>
+@media (min-width: 1024px) {
+}
+</style>
+<script>
+import PopUpMessage from '../components/slots/PopUpMessage.vue'
+export default {
+  components: {PopUpMessage},
+  data() {
+    return {  }
+  },
+}
+</script>
